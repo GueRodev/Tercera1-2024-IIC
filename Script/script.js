e187bfd9e85c60ef722e48a77c67055766d0d692
Guerson  Rodriguez <guerson.rodriguez@outlook.com>
2024-07-02 03:31:36 +0000

    Creacion de Cards

--- Script/script.js
+++ Script/script.js
@@ -9,8 +9,21 @@ const nav = document.querySelector('.nav-links');
 // Añade un 'event listener' al elemento 'burger' que escucha el evento 'click'
 // Cada vez que se haga clic en el icono del menú de hamburguesa, se ejecutará la función proporcionada
 burger.addEventListener('click', () => {
-    // Utiliza el método 'toggle' de 'classList' para añadir o eliminar la clase 'nav-active'
-    // Si la clase 'nav-active' no está presente en el elemento 'nav', 'toggle' la añadirá
-    // Si la clase 'nav-active' ya está presente, 'toggle' la eliminará
     nav.classList.toggle('nav-active');
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+    } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+});
+}
+//transition: transform 0.3s ease;
